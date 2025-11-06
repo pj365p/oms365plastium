beec03a83dc4e035ffa079aeeab6b3ee6a93b45d
pj365p <prashantmjain2610@gmail.com>
2025-11-06 14:20:51 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,10 @@
+// src/firebase.js
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
+// Your Firebase config (from Firebase console)
 const firebaseConfig = {
-  apiKey: "AIzaSyCkFvza_WUdtwVvnqLCzy6xV9zMIcz6t8I",
+  apiKey: "AIzaSyCkFvza_WUdtwVnqLCzy6xV9zMIzcz6t8I",
   authDomain: "oms365plastium-01.firebaseapp.com",
   projectId: "oms365plastium-01",
   storageBucket: "oms365plastium-01.firebasestorage.app",
@@ -10,5 +12,8 @@ const firebaseConfig = {
   appId: "1:249094119811:web:5a1903c987d81bdc92d6b6"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Export the Firestore instance
 export const db = getFirestore(app);
